079817f83d96c259c3cbed7f3555c9793e1f5796
Sanmi Idowu <sanmi@Sanmis-MacBook-Pro.local>
2025-08-16 23:08:26 +0100

    footer added to navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,19 @@
 // frontend/components/Layout.js
 import Navbar from "./Navbar";
+
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar always at top */}
       <Navbar />
-      <main className="py-6">{children}</main>
+
+      {/* Page content grows to fill space */}
+      <main className="flex-grow">{children}</main>
+
+      {/* Sticky footer */}
+      <footer className="bg-gray-100 border-t py-3 text-center text-sm text-gray-600">
+        © 2025 Tech Analytics
+      </footer>
     </div>
   );
 }
